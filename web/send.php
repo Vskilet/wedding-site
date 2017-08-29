c9f0fc9bb01773aa7db89038b17134b1f1c67a84
Victor <victor@sene.ovh>
2017-08-29 10:56:14 +0200

    modification sending parameter

--- web/send.php
+++ web/send.php
@@ -6,9 +6,13 @@
 	$subject = $_POST['Subject'];
 	$message = $_POST['Message'];
 
-	$headers = 'From: contact@constanceetvictor.fr' . "\r\n" .
-	    'Reply-To: contact@constanceetvictor.fr' . "\r\n" .
-	    'X-Mailer: PHP/' . phpversion();
+	$headers = 'From: nous@constanceetvictor.fr' . "\r\n" .
+	'Reply-To: nous@constanceetvictor.fr' . "\r\n" .
+	'X-Mailer: PHP/' . phpversion();
 
-	mail($mail, $subject, $message, $headers);
+	if(mail($mail, $subject, $message, $headers)){
+        //ça marche
+  }else{
+			phpinfo();
+  }
 ?>
